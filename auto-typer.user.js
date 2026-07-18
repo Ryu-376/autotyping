4985f76fd7a4c5c1623cbab1b07109cb39bce7cc
Ryu-376 <hohoho.ahaho@gmail.com>
2026-07-18 14:53:57 +0900

    Create auto-typer.user.js

---
+++ auto-typer.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         タイピング自動化スクリプト
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  対象のタイピングサイトで自動入力を行います
+// @match        https://otonasi-muonn.github.io/typing_game/*
+// @grant        none
+// ==/UserScript==
+
+(async () => {
+    "use strict";
+
+    // ─── ここに、あなたが持っている js を txt に変えたコードを丸ごと貼り付ける ───
+
+})();
